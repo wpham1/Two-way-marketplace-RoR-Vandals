8d5629705233d322b2828e6a76c3e0f367760e0b
Walter <walter.pham@hotmail.com>
2021-08-10 03:35:49 +1000

    added shopping cart, can add and remove items from cart from home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,18 @@ Rails.application.routes.draw do
   resources :listings
   devise_for :users
   root 'home#page'
-
-#  get ':username', to: 'listings#shop'
-#  get ':username/:id', to: 'listings#show'
-
-#  scope ':username' do
-#   resources :listings
-# end
-
+# routes for specific user shops and listings
 resources :listings, path: '/:username/listings', as: :shop do
   put :favourite, on: :member
 end
 
-# resources :listings, only: [:show, :update, :destroy], path: '/:username' do
-#   put :favourite, on: :member
-# end
-
-# have to put favourite on member of listing
-
+# routes for faves
  get ':username/favourites', to: 'listings#fave_list', as: :faves
-# put ':username/favourites', to: 'listings#favourite', as: :faves
+
+post 'listings/add_to_cart/:id', to: 'listings#add_to_cart', as: 'add_to_cart'
+delete 'listings/remove_from_cart/:id', to: 'listings#remove_from_cart', as: 'remove_from_cart'
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
